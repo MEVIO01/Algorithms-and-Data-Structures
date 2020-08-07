32a079f1157486d7e461663e274d416948613e20
MEVIO01 <erik.mirai@outlook.com>
2020-08-07 19:57:41 +0300

    Bubble sort added

---
+++ bubbleSort.py
@@ -0,0 +1,14 @@
+def bubble_sort(arr):
+    arr_length = len(arr)
+
+    for i in range(arr_length):
+        swapped = False
+
+        for j in range(0, arr_length-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+                swapped = True
+        
+        if swapped == False:
+            break
+
